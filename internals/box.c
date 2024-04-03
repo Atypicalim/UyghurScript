79df0b2526d9370277791d08a8cd9bb5474427c1
kompasim <kompasim@ziipin.com>
2024-04-03 18:44:42 +0800

    update test

--- internals/box.c
+++ internals/box.c
@@ -16,11 +16,12 @@ void ug_box_loop(Bridge *bridge)
     for (int i = 0; i < HASHMAP_DEFAULT_CAPACITY; ++i) {
         ptr = map[i].position;
         while (ptr != NULL) {
-            String *key = ptr->key;
+            String *_key = ptr->key;
+            Value *key = Value_newString(_key, NULL);
             Value *val = ptr->value;
             Executer_clearStack(exe);
             Executer_pushStack(exe, val);
-            Executer_pushStack(exe, val);
+            Executer_pushStack(exe, key);
             Executer_runFunc(exe, fun);
             // 
             ptr = ptr->next;
