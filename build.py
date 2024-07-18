3de2f0c2da3cedb5e06436f25b76b4f3a086e9ff
kompasim <kompasim@163.com>
2024-07-18 20:54:08 +0800

    utf8 test

--- build.py
+++ build.py
@@ -9,7 +9,7 @@ tools = builder.tools
 ###############################################################################
 
 VERSION_CODE = 0.2
-SCRIPT_PATH = "./examples/yuguresh.ug"
+SCRIPT_PATH = "./examples/sınav.tr"
 SCRIPT_DIR, SCRIPT_FILE, SCRIPT_EXT, SCRIPT_NAME = tools.tools.parse_path(SCRIPT_PATH)
 
 ###############################################################################
@@ -187,6 +187,7 @@ task.setInput('./main.c')
 task.setLibs([
     "incbin",
     # "string",
+    "utf8",
     "raylib",
     "raygui",
 ])
--- main.c
+++ main.c
@@ -5,6 +5,10 @@
 #include "internals/header.h"
 #include "externals/header.h"
 
+#include "utf8.h"
+#include <locale.h>
+#include <wchar.h>
+
 int main(int argc, char const *argv[])
 {
     INCBIN(Script, UG_SCRIPT_PATH);
@@ -13,13 +17,25 @@ int main(int argc, char const *argv[])
     log_set_level(0);
     log_set_color(true);
     log_set_quiet(false);
+    // 
+    setlocale(LC_ALL, "en_US.utf8");
+    printf("-----------------------------\n");
+    char *txt = "boş";
+    printf("txt: %s \n", txt);
+    printf("sizeof: %i -> %i \n", sizeof(txt), utf8size(txt));
+    printf("strlen: %i -> %i \n", strlen(txt), utf8len(txt));
+    for (size_t i = 0; i < utf8len(txt); i++)
+    {
+        printf("-> %i \n", i);
+    }
+    printf("-----------------------------\n");
     //
-    Uyghur *uyghur = Uyghur_new();
-    register_internal_libraries(uyghur->bridge);
-    register_external_libraries(uyghur->bridge);
-    if (argc == 1) Uyghur_runCode(uyghur, gScriptData, NULL);
-    if (argc >= 2) Uyghur_runArgs(uyghur, argc, argv);
-    Uyghur_free(uyghur);
+    // Uyghur *uyghur = Uyghur_new();
+    // register_internal_libraries(uyghur->bridge);
+    // register_external_libraries(uyghur->bridge);
+    // if (argc == 1) Uyghur_runCode(uyghur, gScriptData, NULL);
+    // if (argc >= 2) Uyghur_runArgs(uyghur, argc, argv);
+    // Uyghur_free(uyghur);
     //
     return 0;
 }
