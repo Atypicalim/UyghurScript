a1b87ba5a24bf86a6dc2da9f16a22f65e41efd34
kompasim <kompasim@163.com>
2024-11-01 20:16:52 +0800

    init voyager

---
+++ uyghur/externals/voyager.c
@@ -0,0 +1,19 @@
+// voyager
+
+#include "../uyghur.c"
+
+// do http get request
+void native_voyager_gain(Bridge *bridge)
+{
+    CString toUrl = Bridge_receiveString(bridge);
+    Value *callback = Bridge_receiveValue(bridge, UG_TYPE_WKR);
+    Bridge_returnEmpty(bridge);
+}
+
+// do http post request
+void native_voyager_give(Bridge *bridge)
+{
+    CString toUrl = Bridge_receiveString(bridge);
+    Value *callback = Bridge_receiveValue(bridge, UG_TYPE_WKR);
+    Bridge_returnEmpty(bridge);
+}
