4ca900bfef796383ccf69155452f9294b6f5e993
atypicalim <atypicalim@163.com>
2024-12-03 00:12:34 +0800

    update script

--- run.py
+++ run.py
@@ -1,4 +1,46 @@
 
+import os
+import sys
+import subprocess
+
+################################################################
+
+__arg1 = sys.argv[1] if len(sys.argv) > 1 else ""
+__arg2 = sys.argv[2] if len(sys.argv) > 2 else ""
+__needUpdate = __arg1 == "--update" or __arg1 == "-u"
+__workingDir = os.path.dirname(os.path.abspath(__file__))
+
+################################################################
+
+def downloadGitTo(url, path):
+    print("\ndownload:", url)
+    if os.path.isdir(path):
+        if  __needUpdate:
+            os.chdir(path)
+            subprocess.run(['git', 'pull'], check=True)
+        else:
+            print("exist!")
+    else:
+        try:
+            subprocess.run(['git', 'clone', url, path], check=True)
+            print("Git clone completed successfully.")
+        except subprocess.CalledProcessError as e:
+            print(f"An error occurred while cloning: {e}")
+        pass
+
+################################################################
+
+downloadGitTo("git@github.com:Atypicalim/my-build-tools.git", "../my-build-tools")
+downloadGitTo("git@github.com:Atypicalim/pure-js-tools.git", "../pure-js-tools")
+downloadGitTo("git@github.com:Atypicalim/c-pure-tools.git", "../c-pure-tools")
+downloadGitTo("git@github.com:Atypicalim/c-xtra-tools.git", "../c-xtra-tools")
+downloadGitTo("git@github.com:Atypicalim/replay.git", "../c-replay-library")
+downloadGitTo("git@github.com:Atypicalim/replot.git", "../c-replot-library")
+
+################################################################
+
+os.chdir(__workingDir)
+
 from scripts.base import *
 import scripts.bind
 # import scripts.converter
--- scripts/interpreter.py
+++ scripts/interpreter.py
@@ -14,26 +14,6 @@ DST_SCRIPT = DST_ALIAS + "." + SCRIPT_EXT
 
 ################################################################
 
-def downloadGitTo(url, path):
-    print("\ndownload:", url)
-    if tools.files.is_folder(path):
-        print("exist!")
-    else:
-        try:
-            subprocess.run(['git', 'clone', url, path], check=True)
-            print("Git clone completed successfully.")
-        except subprocess.CalledProcessError as e:
-            print(f"An error occurred while cloning: {e}")
-        pass
-
-downloadGitTo("git@github.com:Atypicalim/pure-js-tools.git", "../pure-js-tools")
-downloadGitTo("git@github.com:Atypicalim/c-pure-tools.git", "../c-pure-tools")
-downloadGitTo("git@github.com:Atypicalim/c-xtra-tools.git", "../c-xtra-tools")
-downloadGitTo("git@github.com:Atypicalim/replay.git", "../c-replay-library")
-downloadGitTo("git@github.com:Atypicalim/replot.git", "../c-replot-library")
-
-################################################################
-
 def formatVariables(template, names):
     variables = []
     for name in names:
